a9ab1227fffa18fae24b2ad4377612a06f005428
Obed Rayo <obedrayo2356@gmail.com>
2022-12-04 22:38:32 -0500

    Añadiendo los routers

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './core/Home';
-import Search from './core/Search'
+import Signin from './core/Signin'
+import Signup from './core/Signup' 
+import AddCategory from './core/AddCategory' 
+import AddGame from './core/AddGame' 
+import Game from './core/Game'
 
 const RoutesApp = () => {
     return (
         <div>
         <Routes>
                 <Route path="/" element = {<Home/>} />
-                <Route path="/search" element = {<Search/>} />
+                <Route path="/signin" element = {<Signin/>} />
+                <Route path="/signup" element = {<Signup/>} />
+                <Route path="/addcategory" element = {<AddCategory/>} />
+                <Route path="/addgame" element = {<AddGame/>} />
+                <Route path="/games/:videogameId" element = {<Game/>} />
         </Routes>
         </div>
     )
